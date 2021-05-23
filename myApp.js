3bd5d2e2585f508da7f8ef161e31eb560e674213
Bhavin Ag <bhavib.789@gmail.com>
2021-05-23 11:57:37 +0000

    feat: serve static assets in express

--- myApp.js
+++ myApp.js
@@ -3,6 +3,9 @@ var app = express();
 
 console.log("Hello world");
 
+var assetsPath = __dirname + "/public"
+app.use("/public", express.static(assetsPath));
+
 app.get("/", function(req, res,next){
   var absolutePath = __dirname + "/views/index.html"
   res.sendFile(absolutePath);
