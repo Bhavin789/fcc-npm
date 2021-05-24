31e0603a5ccbb3626a96f6b5f01695abe72a7a74
Bhavin Ag <bhavib.789@gmail.com>
2021-05-24 13:55:31 +0000

    feat: add middlewares

--- myApp.js
+++ myApp.js
@@ -4,6 +4,12 @@ var app = express();
 console.log("Hello world");
 
 var assetsPath = __dirname + "/public"
+
+app.use(function(req, res, next) {
+  const {method, path, ip} = req;
+  console.log(`${method} ${path} - ${ip}`);
+  next();
+})
 app.use("/public", express.static(assetsPath));
 
 app.get("/", function(req, res,next){
@@ -11,6 +17,26 @@ app.get("/", function(req, res,next){
   res.sendFile(absolutePath);
 });
 
+app.get("/json", function(req, res,next){
+
+  const messageStyle = process.env['MESSAGE_STYLE'];
+
+  let messageLiteral = "Hello json";
+
+  if( messageStyle === "uppercase"){
+    messageLiteral = messageLiteral.toUpperCase();
+  }
+
+  res.json({"message": messageLiteral});
+
+});
+
+app.get("/now", function(req, res,next){
+  req.time = new Date().toString();
+  next();
+}, function(req, res,next){
+  res.json({"time": req.time});
+});
 
 
 
