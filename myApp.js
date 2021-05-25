782c285d6756025370bff2adf70e1dc043382f20
Bhavin Ag <bhavib.789@gmail.com>
2021-05-25 16:23:00 +0000

    feat: add setup for mongo, wip

--- myApp.js
+++ myApp.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true })
+
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
